2bbc63aa15238fd05218eade458808c5aa4e2932
kento tajiri <k.denjiry@gmail.com>
2021-05-16 14:22:27 +0900

    init

---
+++ integration/main.rs
@@ -0,0 +1,18 @@
+pub mod tests;
+
+fn setup() {
+    println!("Setup")
+}
+
+fn teardown() {
+    println!("Teardown")
+}
+fn main() {
+    // Setup test environment
+    setup();
+
+    // TODO: Run the test
+
+    // Teardown test environment
+    teardown();
+}
